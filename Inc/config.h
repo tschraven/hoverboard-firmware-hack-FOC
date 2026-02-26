51593c2be0e1bdd6a90f606a2f57c294eb0f29a6
tschraven <149015372+tschraven@users.noreply.github.com>
2026-02-26 16:57:18 -0500

    Adjusted PRI_INPUTs_v2

--- Inc/config.h
+++ Inc/config.h
@@ -384,7 +384,7 @@
   #define CONTROL_ADC           0         // use ADC as input. Number indicates priority for dual-input. Disable CONTROL_SERIAL_USART2, FEEDBACK_SERIAL_USART2, DEBUG_SERIAL_USART2!
 
   // #define DUAL_INPUTS                     //  ADC*(Primary) + UART(Auxiliary). Uncomment this to use Dual-inputs
-  #define PRI_INPUT1            2,  1650, 2070, 2480, 40      // TYPE, MIN, MID, MAX, DEADBAND. See INPUT FORMAT section
+  #define PRI_INPUT1            2,  2480, 2070, 1650, 40      // TYPE, MIN, MID, MAX, DEADBAND. See INPUT FORMAT section
   #define PRI_INPUT2            2,  1650, 2070, 2480, 40      // TYPE, MIN, MID, MAX, DEADBAND. See INPUT FORMAT section
   #ifdef DUAL_INPUTS
     #define FLASH_WRITE_KEY     0x1101    // Flash memory writing key. Change this key to ignore the input calibrations from the flash memory and use the ones in config.h
