08264e5c69b73f3a20a92fd652bdb39bdb6a69a2
tschraven <149015372+tschraven@users.noreply.github.com>
2026-03-05 13:43:39 -0500

    Set_FLASHKEY_0x1337

--- Inc/config.h
+++ Inc/config.h
@@ -395,7 +395,7 @@
     #define AUX_INPUT1          3, -1000, 0, 1000, 0  // TYPE, MIN, MID, MAX, DEADBAND. See INPUT FORMAT section
     #define AUX_INPUT2          3, -1000, 0, 1000, 0  // TYPE, MIN, MID, MAX, DEADBAND. See INPUT FORMAT section
   #else
-    #define FLASH_WRITE_KEY     0x1001    // Flash memory writing key. Change this key to ignore the input calibrations from the flash memory and use the ones in config.h
+    #define FLASH_WRITE_KEY     0x1337    // Flash memory writing key. Change this key to ignore the input calibrations from the flash memory and use the ones in config.h Original value 0x1001
     #define DEBUG_SERIAL_USART3           // right sensor board cable, disable if I2C (nunchuk or lcd) is used!
   #endif
 
