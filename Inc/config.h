779b18c7b7b14de59153bcc8ba6cdd74a8136de3
tschraven <149015372+tschraven@users.noreply.github.com>
2026-06-08 18:16:36 -0400

    Speedrate10_speedfilter6500

--- Inc/config.h
+++ Inc/config.h
@@ -264,11 +264,11 @@
 #define DEFAULT_FILTER              13000  // Default for FILTER 0.1f [-] lower value == softer filter [0, 65535] = [0.0 - 1.0].
     // Per-axis base rate (fixdt(1,16,4) units)
     #define DEFAULT_STEER_RATE          100
-    #define DEFAULT_SPEED_RATE          40
+    #define DEFAULT_SPEED_RATE          10
 
     // Per-axis base low-pass filter (fixdt(0,16,16))
     #define DEFAULT_STEER_FILTER        24000
-    #define DEFAULT_SPEED_FILTER        13000
+    #define DEFAULT_SPEED_FILTER        6500
 
     // Allow overriding from elsewhere if ever needed
     #ifndef STEER_RATE
